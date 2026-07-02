3f3232cb3634c8151e4d658f1d68850369e7a7ba
xfredxcore <xfreadxcore@gmail.com>
2026-07-03 00:53:18 +0300

    Updfuck kmain.c

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,22 +1,18 @@
 void kmain(void* mb_struct) {
     (void)mb_struct;
 
-    // Адрес текстового буфера VGA
     unsigned short* vga = (unsigned short*)0xB8000;
 
-    // Очистка экрана (заполнение пробелами с черным фоном)
     for (int i = 0; i < 80 * 25; i++) {
         vga[i] = 0x0F20;
     }
 
-    // Строка "FUCK YOU"
     const char* str1 = "FUCK YOU";
-    int offset1 = (80 * 12) + 36; // Центр экрана (примерно)
+    int offset1 = (80 * 12) + 36;
     for (int i = 0; str1[i] != '\0'; i++) {
         vga[offset1 + i] = 0x0F00 | (unsigned char)str1[i];
     }
 
-    // Строка "and fuck your hardware"
     const char* str2 = "and fuck your hardware";
     int offset2 = (80 * 13) + 29;
     for (int i = 0; str2[i] != '\0'; i++) {
